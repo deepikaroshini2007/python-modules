297798728565c96079d247f30449669f3dcfa781
deepikaroshini2007 <deepikaroshini2007_bcs29@mepcoeng.ac.in>
2026-04-26 16:21:07 +0530

    Create factor_module_main.py
    A simple program that imports a module to compute factors and prime factors of a number.

---
+++ factor_module_main.py
@@ -0,0 +1,7 @@
+import functionmodule
+n=int(input(&quot;enter number:&quot;))
+f=functionmodule.factors(n)
+
+print(&quot;Factors :&quot;,f)
+pf=functionmodule.primeFactors(n)
+print(&quot;Prime Factors :&quot;,pf)
